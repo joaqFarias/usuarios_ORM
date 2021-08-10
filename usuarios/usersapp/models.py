54b9a2c9c89adb9d8139e72a967e50216237e633
Joaquin Farias <jfarias@lythium.cl>
2021-08-10 02:39:10 -0400

    ejercicio terminado

--- usuarios/usersapp/models.py
+++ usuarios/usersapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class users(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email_address = models.EmailField(max_length=255)
+    age = models.IntegerField()
+
+    def __repr__(self) -> str:
+        return f'Nombre: {self.first_name} {self.last_name}, Edad: {self.age}'
